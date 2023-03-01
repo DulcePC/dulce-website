d28b1214ab1b75e592e9f12ef601cdc92732ce99
DulcePC <dulcefrancelispc@gmail.com>
2023-03-01 19:49:05 -0400

    fix: :art: replace static contact information

--- assets/helpers/contact.js
+++
@@ -1,5 +0,0 @@
-const contact = [
-  email = "dulcefrancelispc@gmail.com",
-  linkedin = "https://www.linkedin.com/in/dulceperezsoft",
-  github = "https://github.com/DulcePC"
-]
--- components/molecules/FixedSocialMedia.vue
+++ components/molecules/FixedSocialMedia.vue
@@ -1,16 +1,31 @@
 <template>
   <nav class="w-[50px] h-auto py-6 flex flex-col items-center bg-primary fixed top-1/2 xl:left-0 right-0">
-    <a href="mailto:dulcefrancelispc@gmail.com">
+    <a :href="`mailto:${contact.email}`">
       <img src="/img/email.svg" alt="DulcePC" class="w-3">
     </a>
-    <a href="https://www.linkedin.com/in/dulceperezsoft" target="blank">
+    <a :href="`${contact.linkedin}`" target="blank">
       <img src="/img/linkedin.svg" alt="DulcePC" class="w-3">
     </a>
-    <a href="https://github.com/DulcePC" target="blank">
+    <a :href="`${contact.github}`" target="blank">
       <img src="/img/github.svg" alt="DulcePC" class="w-3">
     </a>
   </nav>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      contact: {
+        email: "dulcefrancelispc@gmail.com",
+        linkedin: "https://www.linkedin.com/in/dulceperezsoft",
+        github: "https://github.com/DulcePC"
+      }
+    }
+  }
+}
+</script>
 
 <style lang="postcss" scoped>
   a { @apply w-8 h-8 mb-6 bg-white rounded-full flex items-center justify-center; }
