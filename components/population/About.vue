6d5fc91d3c3f278ad2b1376be7dbb7cae7bfdc30
DulcePC <dulcefrancelispc@gmail.com>
2023-03-01 20:16:16 -0400

    feat: :sparkles: add about me description

--- components/population/About.vue
+++ components/population/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-secondary bg-opacity-[0.15] pt-20 pb-48">
+  <section class="bg-secondary bg-opacity-[0.15] md:pt-20 md:pb-48 py-8">
     <div class="container">
       <h2 class="mx-auto">
         About me
@@ -10,9 +10,26 @@
       <div class="grid lg:grid-cols-2 grid-cols-1 gap-x-10">
         <!-- Description -->
         <div class="lg:m-0 mb-5">
-          <p class="leading-8 text-base lg:text-left text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <p class="leading-8 text-base lg:text-left text-center mb-2">
+            My primary responsibility is to create and maintain the client-facing portion of websites or web applications, ensuring they are visually appealing, user-friendly, and responsive.
           </p>
+          <p class="leading-8 text-base lg:text-left text-center mb-2">
+            My skills as a junior front-end developer typically include proficiency in HTML, CSS, and JavaScript, also have experience with front-end frameworks and libraries such as Angular and Vue.
+          </p>
+          <p class="leading-8 text-base lg:text-left text-center mb-2">
+            I have worked with several local companies such as October studio and dynamiscreative, currently working as Front end team leader at Kation Interactive
+          </p>
+          <h6 class="font-semibold text-2xl mb-2">My skills</h6>
+          <ul class="text-base lg:text-left text-center flex flex-wrap gap-2">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Tailwindcss</li>
+            <li>JavaScript</li>
+            <li>Vue</li>
+            <li>Angular</li>
+            <li>Ionic</li>
+            <li>Strapi</li>
+          </ul>
         </div>
         <div class="flex items-center">
           <img src="/img/lines.png" alt="Line icon" class="w-14 flex-none mr-6 lg:block hidden">
